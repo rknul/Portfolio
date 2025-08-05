507e7ef720ba06562f2ee89ef8a0f825a2eeac02
Y'Robin B.Knul <yrobinbknul@gmail.com>
2025-08-05 09:54:19 -0400

    Build Command

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
-import { useState } from 'react';
 import Home from './pages/Home/Home';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<BrowserRouter>
